393a49353afb17557fc878ffa18df7694cca89a5
42barracudas <vynohradov_vitaliy@yahoo.com>
2020-05-07 13:06:46 +0000

    Update Create_APIv3.ps1

--- Create_APIv3.ps1
+++ Create_APIv3.ps1
@@ -1,46 +1,42 @@
 ﻿Import-Module BitsTransfer
 $prefix=Read-Host ″Enter POC Prefix″
-Write-Host '1 = ORB-EMEA-SQL1\SQL2012'
-Write-Host '2 = ORB-EMEA-SQL1\SQL2016'
-switch(read-host "Select Instance"){
-1 {$ServerName = 'ORB-EMEA-SQL1\SQL2012'}
-2 {$ServerName = 'ORB-EMEA-SQL1\SQL2016'}
-default {$ServerName = 'ORB-EMEA-SQL1\SQL2012'}
+Write-Host '1 = ORB-US-SQL1\SQL2012'
+Write-Host '2 = ORB-US-SQL1\SQLSERVER2016'
+switch(read-host "Select Instance where iServerDB is hosted"){
+1 {$ServerName = 'ORB-US-SQL1\SQL2012'}
+2 {$ServerName = 'ORB-US-SQL1\SQLSERVER2016'}
+default {$ServerName = 'ORB-US-SQL1\SQL2012'}
 }
-# IIS/PORTAL
-$dirName = "F:\Portal\$prefix.APIv3.0.1"
-#$serviceDirName = "F:\Portal\$prefix\Service"
+# IIS/API
+$dirName = "E:\Portal\$prefix.APIv3.0.2"
 $Binding = "*:80:"+"$Prefix"+"api.orbuscloud.com"
-#create Portal Folder
+#create API Folder
 if(-not (test-path $dirName)) {
     md $dirName | out-null
 } else {
     write-host "$dirName already exists!"
 }
 
-#Path to Portal files Template (F:\Portal\iServer API 3.0.1) and execute copy
-$source1='F:\Portal\iServer API 3.0.1\*'
+#Path to API files Template (E:\Portal\iServer API 3.0.2) and execute copy
+$source1='E:\Portal\iServer API 3.0.2\*'
 Copy-Item -path $source1 -destination $dirName -recurse
 
-# replace contents of copied Portal files
+# replace contents of copied API files
 $current = 'XYZ'
-#(get-content "$dirName\API\Web.config").replace("$current", "$prefix") | set-content $dirName\API\Web.config
-#(get-content "$dirName\Service\db.config").replace("$current", "$prefix") | set-content $dirName\Service\db.config
-$APIwebConfig = "$dirName\API\Web.config"
+$APIwebConfig = "$dirName\Web.config"
 $doc = (Get-Content $APIwebConfig) -as [Xml]
 $dbconnectiondetails = $doc.SelectSingleNode('configuration/connectionStrings/add')
 $dbconnectiondetails.setattribute("connectionString","Data Source=$ServerName;Initial Catalog=$prefix-iServerDB;User Id=portaluser;Password=portaluser;")
-$doc.save("$dirName\API\Web.config")
+$doc.save("$dirName\Web.config")
 
 Write-Host 'API config files updated'
 
 # create Website in IIS
 Import-Module WebAdministration
-$iisAppPoolName = "$Prefix.APIv3.0.1"
+$iisAppPoolName = "$Prefix.APIv3.0.2"
 $iisAppPoolDotNetVersion = "v4.0"
-$iisAppName = "$Prefix.APIv3.0.1"
-$directoryPath = "$dirName\API"
-#$serviceDirName = "$dirName/Service"
+$iisAppName = "$Prefix.APIv3.0.2"
+$directoryPath = "$dirName"
 
 #navigate to the app pools root
 cd IIS:\AppPools\
